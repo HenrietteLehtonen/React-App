9d6cd186921a0efb16d5d95a8466ae8573315848
Henriette Lehtonen <Henriette.Lehtonen@student.lab.fi>
2025-01-22 09:51:07 +0200

    dialog update

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,7 @@
 import {MediaItem} from 'hybrid-types/DBTypes';
-import MediaRow from './mediarow';
+import MediaRow from './MediaRow';
+import SingleView from './SingleView';
+import {useState} from 'react';
 
 const mediaArray: MediaItem[] = [
   {
@@ -42,8 +44,16 @@ const mediaArray: MediaItem[] = [
 ];
 
 const Home = () => {
+  const [SelectedItem, setSelectedItem] = useState<MediaItem | undefined>(
+    undefined,
+  );
+
   return (
     <>
+      {SelectedItem && (
+        <SingleView item={SelectedItem} setSelectedItem={setSelectedItem} />
+      )}
+
       <h2>My Media</h2>
       <table>
         <thead>
@@ -58,7 +68,11 @@ const Home = () => {
         </thead>
         <tbody>
           {mediaArray.map((item) => (
-            <MediaRow key={item.media_id} item={item} />
+            <MediaRow
+              key={item.media_id}
+              item={item}
+              setSelectedItem={setSelectedItem}
+            />
           ))}
         </tbody>
       </table>
---
+++ src/components/SingleView.tsx
@@ -0,0 +1,36 @@
+import {MediaItem} from 'hybrid-types/DBTypes';
+
+const SingleView = (props: {
+  item: MediaItem | undefined;
+  setSelectedItem: (item: MediaItem | undefined) => void;
+}) => {
+  const {item, setSelectedItem} = props;
+  console.log(item);
+  return (
+    <>
+      <dialog open>
+        {item && (
+          <>
+            <button
+              onClick={() => {
+                setSelectedItem(undefined);
+              }}
+            >
+              Close
+            </button>
+            <h1>{item.media_type}</h1>
+            {item.media_type.includes('image') ? (
+              <img src={item.filename} alt={item.title} />
+            ) : (
+              <video src={item.filename} controls />
+            )}
+            <h1>{item.title}</h1>
+            <h2>{item.description}</h2>
+          </>
+        )}
+      </dialog>
+    </>
+    //
+  );
+};
+export default SingleView;
